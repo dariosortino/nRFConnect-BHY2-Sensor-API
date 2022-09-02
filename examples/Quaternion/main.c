87c0500cd4e1b3205c77e85d50f995c941a1bd05
Dario Sortino <dario.sortino@iit.it>
2022-09-02 14:02:30 +0200

    Added parse functions

--- examples/Quaternion/main.c
+++ examples/Quaternion/main.c
@@ -11,7 +11,7 @@
 #include <logging/log.h>
 
 #include "bhy2.h"
-#include "bhy2_parse.h"
+#include "parse.h"
 #include "common.h"
 
 #include "Bosch_APP30_SHUTTLE_BHI260_aux_BMM150.fw.h"
@@ -37,8 +37,6 @@
 #define QUAT_SENSOR_ID     BHY2_SENSOR_ID_RV_WU
 
 
-static void parse_quaternion(const struct bhy2_fifo_parse_data_info *callback_info, void *callback_ref);
-static void parse_meta_event(const struct bhy2_fifo_parse_data_info *callback_info, void *callback_ref);
 static void print_api_error(int8_t rslt, struct bhy2_dev *dev);
 
 static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
@@ -63,6 +61,7 @@ int main(void)
 	static uint8_t work_buffer[WORK_BUFFER_SIZE];
 	uint8_t hint, hintr_ctrl, hif_ctrl, boot_status;
 	static struct bhy2_dev bhy2;
+    uint8_t cbref;
 	
 	rslt = bhy2_init(BHY2_I2C_INTERFACE, bhy2_i2c_read, bhy2_i2c_write, bhy2_delay_us, MAX_READ_WRITE_LEN, NULL, &bhy2);
 	print_api_error(rslt, &bhy2);
@@ -143,9 +142,9 @@ int main(void)
 			printk("Boot successful. Kernel version %u.\r\n", version);
 		}
         
-		rslt = bhy2_register_fifo_parse_callback(BHY2_SYS_ID_META_EVENT, parse_meta_event, NULL, &bhy2);
+		rslt = bhy2_register_fifo_parse_callback(BHY2_SYS_ID_META_EVENT, parse_meta_event, (void*)&cbref, &bhy2);
 		print_api_error(rslt, &bhy2);
-		rslt = bhy2_register_fifo_parse_callback(BHY2_SYS_ID_META_EVENT_WU, parse_meta_event, NULL, &bhy2);
+		rslt = bhy2_register_fifo_parse_callback(BHY2_SYS_ID_META_EVENT_WU, parse_meta_event, (void*)&cbref, &bhy2);
 		print_api_error(rslt, &bhy2);
 		rslt = bhy2_register_fifo_parse_callback(QUAT_SENSOR_ID, parse_quaternion, NULL, &bhy2);
 		print_api_error(rslt, &bhy2);
@@ -172,137 +171,25 @@ int main(void)
 
     while (1)
     {
-		a++;
-		if(a==10000) a = 0;
 		ret = gpio_pin_toggle_dt(&led);
-		// if (a<10) printk("Printing test %d\n", a);
 		if (ret < 0) {
 			return ret;
 		}
-        // Data from the FIFO is read and the relevant callbacks if registered are called
-        if(bhy2_get_interrupt_status(&hint, &bhy2))
+        
+        rslt = bhy2_get_interrupt_status(&hint, &bhy2);
+        print_api_error(rslt, &bhy2);
+
+        if(hint)
         {
+            // Data from the FIFO is read and the relevant callbacks if registered are called
             rslt = bhy2_get_and_process_fifo(work_buffer, WORK_BUFFER_SIZE, &bhy2);
             print_api_error(rslt, &bhy2);		
         }
-		k_msleep(SLEEP_TIME_MS);
     }
     return rslt;
 
 }
 
-static void parse_quaternion(const struct bhy2_fifo_parse_data_info *callback_info, void *callback_ref)
-{
-    (void)callback_ref;
-    struct bhy2_data_quaternion data;
-    uint32_t s, ns;
-    if (callback_info->data_size != 11) // Check for a valid payload size. Includes sensor ID 
-    {
-        return;
-    }
-
-    bhy2_parse_quaternion(callback_info->data_ptr, &data);
-
-    uint64_t timestamp = *callback_info->time_stamp; //  Store the last timestamp 
-
-    timestamp = timestamp * 15625; // Timestamp is now in nanoseconds
-    s = (uint32_t)(timestamp / UINT64_C(1000000000));
-    ns = (uint32_t)(timestamp - (s * UINT64_C(1000000000)));
-
-    /*printk("SID: %u; T: %u.%09u; x: %f, y: %f, z: %f, w: %f; acc: %.2f\r\n",
-           callback_info->sensor_id,
-           s,
-           ns,
-           data.x / 16384.0f,
-           data.y / 16384.0f,
-           data.z / 16384.0f,
-           data.w / 16384.0f,
-           ((data.accuracy * 180.0f) / 16384.0f) / 3.141592653589793f);*/
-    printk("%f %f %f %f\n",
-        data.w / 16384.0f,
-        data.x / 16384.0f,
-        data.y / 16384.0f,
-        data.z / 16384.0f);
-}
-
-static void parse_meta_event(const struct bhy2_fifo_parse_data_info *callback_info, void *callback_ref)
-{
-    (void)callback_ref;
-    uint8_t meta_event_type = callback_info->data_ptr[0];
-    uint8_t byte1 = callback_info->data_ptr[1];
-    uint8_t byte2 = callback_info->data_ptr[2];
-    char *event_text;
-
-    if (callback_info->sensor_id == BHY2_SYS_ID_META_EVENT)
-    {
-        event_text = "[META EVENT]";
-    }
-    else if (callback_info->sensor_id == BHY2_SYS_ID_META_EVENT_WU)
-    {
-        event_text = "[META EVENT WAKE UP]";
-    }
-    else
-    {
-        return;
-    }
-
-    switch (meta_event_type)
-    {
-        case BHY2_META_EVENT_FLUSH_COMPLETE:
-            printk("%s Flush complete for sensor id %u\r\n", event_text, byte1);
-            break;
-        case BHY2_META_EVENT_SAMPLE_RATE_CHANGED:
-            printk("%s Sample rate changed for sensor id %u\r\n", event_text, byte1);
-            break;
-        case BHY2_META_EVENT_POWER_MODE_CHANGED:
-            printk("%s Power mode changed for sensor id %u\r\n", event_text, byte1);
-            break;
-        case BHY2_META_EVENT_ALGORITHM_EVENTS:
-            printk("%s Algorithm event\r\n", event_text);
-            break;
-        case BHY2_META_EVENT_SENSOR_STATUS:
-            printk("%s Accuracy for sensor id %u changed to %u\r\n", event_text, byte1, byte2);
-            break;
-        case BHY2_META_EVENT_BSX_DO_STEPS_MAIN:
-            printk("%s BSX event (do steps main)\r\n", event_text);
-            break;
-        case BHY2_META_EVENT_BSX_DO_STEPS_CALIB:
-            printk("%s BSX event (do steps calib)\r\n", event_text);
-            break;
-        case BHY2_META_EVENT_BSX_GET_OUTPUT_SIGNAL:
-            printk("%s BSX event (get output signal)\r\n", event_text);
-            break;
-        case BHY2_META_EVENT_SENSOR_ERROR:
-            printk("%s Sensor id %u reported error 0x%02X\r\n", event_text, byte1, byte2);
-            break;
-        case BHY2_META_EVENT_FIFO_OVERFLOW:
-            printk("%s FIFO overflow\r\n", event_text);
-            break;
-        case BHY2_META_EVENT_DYNAMIC_RANGE_CHANGED:
-            printk("%s Dynamic range changed for sensor id %u\r\n", event_text, byte1);
-            break;
-        case BHY2_META_EVENT_FIFO_WATERMARK:
-            printk("%s FIFO watermark reached\r\n", event_text);
-            break;
-        case BHY2_META_EVENT_INITIALIZED:
-            printk("%s Firmware initialized. Firmware version %u\r\n", event_text, ((uint16_t)byte2 << 8) | byte1);
-            break;
-        case BHY2_META_TRANSFER_CAUSE:
-            printk("%s Transfer cause for sensor id %u\r\n", event_text, byte1);
-            break;
-        case BHY2_META_EVENT_SENSOR_FRAMEWORK:
-            printk("%s Sensor framework event for sensor id %u\r\n", event_text, byte1);
-            break;
-        case BHY2_META_EVENT_RESET:
-            printk("%s Reset event\r\n", event_text);
-            break;
-        case BHY2_META_EVENT_SPACER:
-            break;
-        default:
-            printk("%s Unknown meta event with id: %u\r\n", event_text, meta_event_type);
-            break;
-    }
-}
 
 static void print_api_error(int8_t rslt, struct bhy2_dev *dev)
 {
